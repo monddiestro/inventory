0083821d73a65b82bf56c1a79fc5975159629ad0
Mond Diestro <reymonddiestro@gmail.com>
2020-01-30 15:47:39 +0800

    create base template

---
+++ application/views/script.php
@@ -0,0 +1,13 @@
+  <!-- Bootstrap core JavaScript-->
+  <script src="<?php echo base_url('src/js/jquery.min.js') ?>"></script>
+  <script src="<?php echo base_url('src/js/bootstrap.bundle.min.js') ?>"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="<?php echo base_url('src/js/jquery.easing.min.js') ?>"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="<?php echo base_url('src/js/sb-admin-2.min.js') ?>"></script>
+
+  </body>
+
+</html>
